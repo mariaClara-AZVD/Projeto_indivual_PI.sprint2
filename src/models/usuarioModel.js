57ed606b4ded2cef5520fc6452a23c431c47fcc7
mariaClara-AZVD <maria.azevedo@sptech.school>
2026-06-02 07:37:28 -0300

    Update usuarioModel.js

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -17,12 +17,12 @@ function autenticar(email, senha) {
     return database.executarComParametros(instrucaoSql, [email, senha]);
 }
 
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+// Coloca os mesmos parâmetros aqui!
 function cadastrar(nome, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
+    // Insere exatamente a query do banco aqui! lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados
     var instrucaoSql = `
         INSERT INTO usuario
         (nome_usuario, email, senha)
